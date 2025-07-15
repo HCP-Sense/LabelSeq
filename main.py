108f7bc73f992f9e3a3367b98dd4d7ef915bc61b
Johannes Bayer <bayer@hcp-sense.com>
2025-07-15 15:41:10 +0200

    Added Main Skeleton

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# Third-Party Import
+import wx
+
+
+app = wx.App() 
+window = wx.Frame(None, title = "LabelSeq", size = (300,200)) 
+panel = wx.Panel(window)
+label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
+window.Show(True) 
+app.MainLoop()
