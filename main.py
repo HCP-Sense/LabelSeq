63fb59ed8ff721f3b04cd2539c926844a94e7f07
Johannes Bayer <bayer@hcp-sense.com>
2025-07-15 18:38:57 +0200

    draw_sequence() Adds Lines

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import wx
 from wx.lib.floatcanvas import FloatCanvas
+from wx.lib.floatcanvas.FCObjects import Line
 
 
 app = wx.App() 
@@ -25,13 +26,15 @@ canvas = FloatCanvas.FloatCanvas(window, -1,
 
 def draw_sequence(seq: np.ndarray):
 
-
-    # add a circle
     for step, value in enumerate(seq):
-
-        cir = canvas.AddCircle((step*10, value*10), 5)
+        # add a circle
+        cir = canvas.AddCircle((step*10, value*10), 5, LineWidth=0, FillColor="Blue")
         canvas.AddObject(cir)
 
+    for step, (value_a, value_b) in enumerate(zip(seq[:-1], seq[1:])):
+        print(f"{step} - {value_a} - {value_b}")
+        line = Line(np.array([[step*10,value_a*10],[(step+1)*10,value_b*10]]))
+        canvas.AddObject(line)
 
 
 seq = np.random.rand(20)
@@ -40,8 +43,8 @@ print(seq)
 draw_sequence(seq)
 
 # add a rectangle
-rect = FloatCanvas.Rectangle((110, 10), (100, 100), FillColor='Red')
-canvas.AddObject(rect)
+#rect = FloatCanvas.Rectangle((110, 10), (100, 100), FillColor='Red')
+#canvas.AddObject(rect)
 
 canvas.Draw()
 
