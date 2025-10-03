e06ccc9315687de15354f171efed4770b3d86c3e
Glein <moyegp@gmail.com>
2025-10-03 20:02:07 +0200

    function to load different file formats

---
+++ LoadFile.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import filedialog
+from pathlib import Path
+import pandas as pd
+import librosa
+
+
+def load_file():
+    # create the hidden root window first
+    root = tk.Tk()
+    root.withdraw()
+
+    # open file dialog
+    file_path = filedialog.askopenfilename(
+        title="Select a data file",
+        filetypes=[
+            ("CSV files", "*.csv"),
+            ("Audio files", "*.wav;*.mp3;*.aiff"),
+            ("Excel files", "*.xls;*.xlsx"),
+            ("JSON files", "*.json"),
+            ("All files", "*.*")
+        ]
+    )
+
+    if not file_path:
+        print("No file selected.")
+        return None
+
+    ext = Path(file_path).suffix.lower()
+    if ext in [".csv", ".txt"]:
+        file_content = pd.read_csv(file_path)
+    elif ext in [".xls", ".xlsx"]:
+        file_content = pd.read_excel(file_path, engine="openpyxl")
+    elif ext == ".json":
+        file_content = pd.read_json(file_path)
+    elif ext in [".wav", ".mp3", ".aiff"]:
+        file_content, sr = librosa.load(file_path, sr=None)
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
+
+
+    file_content = file_content[0:10000]
+    file_content = file_content * 100
+ 
+    return file_content
+
+
+
